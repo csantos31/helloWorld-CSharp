911f9b1eff4f20232852de0ff6b9664afb66a4a1
Cyntia Santos <cyntia.aparecida@hotmail.com>
2020-12-09 11:16:58 -0300

    brincando com variáveis

---
+++ 2-CriandoVariaveis/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 2 - Criando Variáveis");
+
+            int idade = 21;
+            Console.WriteLine(idade);
+            
+            Console.WriteLine("Execução finalizada - tecle enter para sair. . .");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_CriandoVariaveisPontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 3 - Criando Variáveis ponto flutuante");
+
+            double salario = 3000.50;
+            Console.WriteLine(salario);
+
+            salario = 3002 / 4;
+            Console.WriteLine("\n\t" + salario + " sem casa decimal");
+
+            salario = 3002.0 / 4;
+            Console.WriteLine("\n\t" + salario + " com casa decimal");
+
+
+
+            Console.WriteLine("Execução finalizada - tecle enter para sair. . .");
+            Console.ReadLine();
+        }
+    }
+}
