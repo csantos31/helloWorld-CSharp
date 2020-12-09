50c6e6272ff8f19e67d1062d20d92dad3ca0d219
Cyntia Santos <cyntia.aparecida@hotmail.com>
2020-12-09 12:43:21 -0300

    brincando com strings

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 5 - Caracteres e textos");
+
+            char primeiraLetra = 'a';
+            Console.WriteLine(primeiraLetra);
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            String titulo = "A aula é de strings e chars - caracteres - " + 2020;
+            Console.WriteLine(titulo);
+
+            titulo = @"Os cursos oferecidos são: 
+- Java
+- .Net
+- Javascript";
+            Console.WriteLine(titulo);
+
+            Console.ReadLine();
+        }
+    }
+}
